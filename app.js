af8ad7a5310ffe366754dc866252f44d5dd227d6
Steven Williams <ste.d.williams@gmail.com>
2013-03-04 08:37:31 -0600

    set Redis list for log entries

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ Tail = require('tail').Tail;
 tail = new Tail("/var/log/system.log");
 
 tail.on("line", function (data) {
-  //client.hset("nid", "log", data, redis.print);
+  client.hset("nid", "log", data, redis.print);
   //console.log(data);
 });
